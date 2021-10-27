b07715a6bbd883767117965d09eec37c35dcde1c
Bob F <bobfdev@gmail.com>
2021-10-27 15:22:31 -0500

    added redux dependencies

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,20 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialAuthState = {
-  isAuthenticated: false,
+    isAuthenticated: false,
 };
 
 const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
+        },
+    }
 });
 
 export const authActions = authSlice.actions;
